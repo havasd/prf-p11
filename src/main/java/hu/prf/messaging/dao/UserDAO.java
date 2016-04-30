01bcf516ae3428ef236e9545a8a32e7b222aaf4e
dorcsi <erdosi.dorottya@gmail.com>
2016-04-30 10:37:13 +0200

    group: creating and listing pages

--- src/main/java/hu/prf/messaging/dao/UserDAO.java
+++ src/main/java/hu/prf/messaging/dao/UserDAO.java
@@ -65,20 +65,21 @@ public class UserDAO extends GenericDAO<User, Long> {
 				"where m.reciever.id = :uid and m.seen = true " +
 				"order by m.date" +
 			")",
-			/*+ " union "
-			+ "select u " +
-			"from User u " +
-			"where u in (" +
-			"select m.reciever " +
-			"from Message m " +
-			"where m.sender.id = :uid " +
-			"order by m.date" +
-			")",*/
 			User.class);
 		q.setParameter("uid", session.getUserId());
 		List<User> l1= q.getResultList();
 		List<User> l2=getOldMessagePartners2();
-		l1.addAll(l2);
+		boolean add=true;
+		for(User u2:l2){
+			for(User u1:l1){
+				if(u1.getId()==u2.getId())
+					add=false;
+			}
+			if(add)
+				l1.add(u2);
+			else
+				add=true;
+		}
 		return l1;
 	}
 	public List<User> getOldMessagePartners2() {
